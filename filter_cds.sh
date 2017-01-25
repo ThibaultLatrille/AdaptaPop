a479553c9853a4954c2f56e5b1529174e9deb3fe
Thibault Latrille <thibault.latrille@ens-lyon.fr>
2017-01-25 17:16:25 +0100

    bash script to launch the analysis (filter the SNPs and compute the Dn/Ds and the Pn/Ps)

--- filter_cds.sh
+++ filter_cds.sh
@@ -1,3 +1,5 @@
-cd /panhome/tlatrill
-python ./create_interval_cds.py
-bedtools intersect -a ./Homo_sapiens_79_polymorphism.vcf -b ./79_interval_cds.bed -wb > ./Homo_sapiens_79_polymorphism_in_cds.vcf
+# cd /panhome/tlatrill/AdaptaPop
+cd ~/AdaptaPop
+python3 ./create_interval_cds.py
+bedtools intersect -a ./data/Homo_sapiens_79_polymorphism.vcf -b ./data/79_interval_cds.bed -wb > ./data/Homo_sapiens_79_polymorphism_in_cds.vcf
+python3 ./mk_test_cds.py
