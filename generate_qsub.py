58b8290b40cb12bdf97d4042048ee8c05d603633
Thibault Latrille <thibault.latrille@ens-lyon.fr>
2017-01-30 15:27:00 +0100

    Data path fix for qsub

--- generate_qsub.py
+++ generate_qsub.py
@@ -2,8 +2,10 @@ import os
 
 path = "om_79_cds_mammals_no_pan_marsu"
 tree = "mammals_no_homi_marsu.tree"
+data = "/pandata/tlatrill/AdaptaPop/data"
+# data = "./data"
 
-for file in os.listdir("./data/" + path):
+for file in os.listdir(data + "/" + path):
     if file.endswith("filtered_NT.ali"):
         file_name = file[:-4]
         qsub = open("./qsub/" + file_name + ".pbs", 'w')
@@ -17,12 +19,12 @@ for file in os.listdir("./data/" + path):
         qsub.writelines("#PBS -W umask=022\n")
         qsub.writelines("#PBS -r n\n")
         qsub.writelines("ulimit ­v 4096000 ­T 8\n")
-        qsub.writelines("cp /pandata/tlatrill/AdaptaPop/data/" + path + "/" + file + " ./" + file + "\n")
-        qsub.writelines("cp /pandata/tlatrill/AdaptaPop/data/" + tree + " ./" + tree + "\n")
+        qsub.writelines("cp " + data + "/" + path + "/" + file + " ./" + file + "\n")
+        qsub.writelines("cp " + data + "/" + tree + " ./" + tree + "\n")
         command = "mpirun -n 8 /panhome/tlatrill/pbmpi/data/pb_mpi -f -x 1 10 -s -d ./" + file
         command += " -T ./" + tree + " -mutsel -dp ./" + file_name + "\n"
         qsub.writelines(command)
-        qsub.writelines("cp ./" + file_name + ".* /pandata/tlatrill/AdaptaPop/data/pbmpi")
+        qsub.writelines("cp ./" + file_name + ".* " + data + "/pbmpi")
         qsub.close()
 
 print('Job completed')
