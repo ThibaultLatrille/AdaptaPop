00409d003a3ab1b058089093175cc442b4a3240e
Thibault Latrille <thibault.latrille@ens-lyon.fr>
2017-02-07 16:04:01 +0100

    Qsub export the TMPDIR and uses the data directly in pandata

--- generate_qsub.py
+++ generate_qsub.py
@@ -14,18 +14,22 @@ for file in os.listdir(data + "/" + path):
         qsub.writelines("#\n")
         qsub.writelines("#PBS -q q1day\n")
         qsub.writelines("#PBS -l nodes=1:ppn=8,mem=4gb\n")
-        qsub.writelines("#PBS -o /pandata/tlatrill/out" + file_name + "\n")
-        qsub.writelines("#PBS -e /pandata/tlatrill/err" + file_name + "\n")
+        qsub.writelines("#PBS -o /pandata/tlatrill/out_err/out" + file_name + "\n")
+        qsub.writelines("#PBS -e /pandata/tlatrill/out_err/err" + file_name + "\n")
         qsub.writelines("#PBS -j oe\n")
         qsub.writelines("#PBS -W umask=022\n")
         qsub.writelines("#PBS -r n\n")
-        qsub.writelines("cp " + data + "/" + path + "/" + file + " ./" + file + "\n")
-        qsub.writelines("cp " + data + "/" + tree + " ./" + tree + "\n")
+        qsub.writelines("#PBS -r n\n")
+        qsub.writelines("TMP=/tmp/tlatrill$RANDOM\n")
+        qsub.writelines("export TMPDIR=$TMP\n")
         for chain in ["_1", "_2"]:
-            command = "mpirun -n 8 ~/pbmpi/data/pb_mpi -f -x 1 200 -s -d ./" + file
-            command += " -T ./" + tree + " -mutsel -dp ./" + file_name + chain + "\n"
+            command = "mpirun -n 8 ~/pbmpi/data/pb_mpi -f -mutsel -freeomega -dp -s -x 1 200"
+            command += " -d " + data + "/" + path + "/" + file
+            command += " -T " + data + "/" + tree
+            command += " ./" + file_name + chain + "\n"
             qsub.writelines(command)
             qsub.writelines("mv ./" + file_name + chain + ".* " + data + "/pb_output\n")
+        qsub.writelines("rm -rf $TMP\n")
         qsub.close()
 
 print('Job completed')
