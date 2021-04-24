ede9150fb9da419e351a618da4db0139fc38b96c
ThibaultLatrille <thibault.latrille@univ-lyon1.fr>
2021-04-24 22:23:47 +0200

    Removed legacy scripts

--- pipeline/filter_cds.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# cd /panhome/tlatrill/AdaptaPop
-cd ~/AdaptaPop
-python3 ./write_bedfile.py
-bedtools intersect -a ./data/Homo_sapiens_88_GRCh38_polymorphism.vcf -b ./data/88_GRCh38_interval_cds.bed -wb > ./data/Homo_sapiens_88_GRCh38_polymorphism_in_cds.vcf
-
-python3 ./write_estimates_snp.py
