127cd873a0f295380635458e58e67e1618570e4b
ThibaultLatrille <thibault.latrille@univ-lyon1.fr>
2021-07-15 14:33:44 +0200

    Refactored and clean manuscript folder, init supplementary materials

---
+++ copy_artworks.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+\cp -rf ./Contrasts/Grapes+PolyDFE/Analysis/* ./manuscript/artworks/
+\cp -f ./Contrasts/scatterplot_gene.pdf ./manuscript/artworks/
+\cp -f ./Contrasts/scatterplot_site.pdf ./manuscript/artworks/
