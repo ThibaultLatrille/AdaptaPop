61b3486310ebe515a64dcbdf8fbacb895a003cc5
Thibault Latrille <thibault.latrille@ens-lyon.org>
2022-12-20 15:39:30 +0100

    Removed the partition for slurm

--- OrthoMam/snakeslurm.sh
+++ OrthoMam/snakeslurm.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-snakemake --forcerun -k --printshellcmds -j 500 --cluster "sbatch -J OrthoMam{params.name} -p {params.queue} -N 1 -o ./slurm/%x.%j.out -e ./slurm/%x.%j.err --cpus-per-task={params.threads} --mem={params.mem} -t {params.time}"
+snakemake --forcerun -k --printshellcmds -j 512 --cluster "sbatch -J OrthoMam{params.name} -p cpu -N 1 -o ./slurm/%x.%j.out -e ./slurm/%x.%j.err --cpus-per-task={params.threads} --mem={params.mem} -t {params.time}"
